a9ad915150a6c3ceaf04ced931a1dcf14708c1db
kd2133 <dakovic.karlo@gmail.comgit config user.email dakovic.karlo@gmail.com>
2026-07-15 18:32:24 +0200

    Add is_neighbor(), remove_walls and apply_logo()

--- a-maze-ing.py
+++ a-maze-ing.py
@@ -1,6 +1,7 @@
 import sys
 import traceback
 from src.config.parser import parse_config
+from src.generator.maze_generator import MazeGenerator
 
 # except Exception also sicherheitsnetz fuer unerwartete bugs, und traceback damit wir sehen was es ist weil Exception auch unsere coding Fehler handled und wir ohne traceback vielleicht nicht drauf kommen wuerden?
 # noch hinzufuegen maybe: prints auf stderr statt stdout, und sys.exit() nach exception?
@@ -8,7 +9,20 @@ def main() -> None:
     try:
         filename = "config.txt"
         config = parse_config(filename)
+        maze = MazeGenerator(config)
+        #cell = maze.grid[2][2]
+        #maze.grid[2][3].visited = True
+        #cell_visited = maze.grid[0][1]
 
+        #neighbors = maze.is_neighbor(cell)
+        #for key in neighbors:
+            #print(f"{key}: {neighbors[key].y}, {neighbors[key].x}")
+
+        current_cell = maze.grid[2][2]
+        next_cell = maze.grid[2][3]
+        maze.remove_walls(current_cell, next_cell)
+        print(current_cell.walls)
+        print(next_cell.walls)
 
 
     except FileNotFoundError:
--- src/config/maze_config.py
+++ src/config/maze_config.py
@@ -16,10 +16,10 @@ class MazeConfig():
             raise ValueError(f"Height/ Width must be higher than 0, height: {self.height}, width: {self.width}")
         if self.entry_pos == self.exit_pos:
             raise ValueError(f"Entry/ exit can't be equal, entry: {self.entry_pos}, exit: {self.exit_pos}")
-        x, y = self.entry_pos
+        y, x = self.entry_pos
         if x < 0 or x >= self.width or y < 0 or y >= self.height:
             raise ValueError(f"Entry {self.entry_pos} outside the maze.") 
-        x, y = self.exit_pos
+        y, x = self.exit_pos
         if x < 0 or x >= self.width or y < 0 or y >= self.height:
             raise ValueError(f"Exit {self.exit_pos} outside the maze.")
         
